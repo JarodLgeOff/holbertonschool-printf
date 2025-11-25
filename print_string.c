a624c8b4413344608cd72a5450d5093813c4cde6
Luken <luken@laptop-hmlsaa6l.home>
2025-11-25 10:51:54 +0100

    feat : create a function that print a string

---
+++ print_string.c
@@ -0,0 +1,21 @@
+#include "project_printf.h"
+/**
+ * print_char - prints a character
+ * 
+ */
+int print_string(va_list s)
+{
+	char *my_string;
+	int  i = 0;
+
+	my_string = va_arg(s, char *);
+	if (my_string == NULL)
+		my_string = "(null)";
+
+	while (my_string[i])
+	{
+		_putchar(my_string[i]);
+		i++;
+	}
+	return (i);
+}
