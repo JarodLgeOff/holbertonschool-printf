ba0b6c4cd620ca0acb1483229678a55c48386e21
Luken <luken@laptop-hmlsaa6l.home>
2025-11-25 14:54:43 +0100

    feat: Implement print_deci function to print decimal integers

---
+++ print_deci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_deci - prints a decimal integer
+ * @args: list of arguments
+ * 
+ * Return: number of characters printed
+ */
+int print_deci(va_list args)
+{
+	int n = va_arg(args, int);
+	int num = n;
+	int div = 1;
+	int count = 0;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -num;
+	}
+	while ((num / div) >= 10) 
+		div *= 10;
+
+	while (div > 0)
+	{
+		_putchar ((num / div) + '0');
+		count++;
+		num %= div;
+		div /= 10;
+	}
+	return (num);
+}
