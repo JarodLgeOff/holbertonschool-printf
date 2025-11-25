6c44f866b94b337a95d79e64289ee85f499848a9
JarodLge <contact.jarodlge@gmail.com>
2025-11-25 09:40:27 +0100

    fix: Correct function declaration for find_function and add specifier struct

--- project_printf.h
+++ project_printf.h
@@ -6,8 +6,21 @@
 #include <unistd.h>
 #include <stddef.h>
 
-int (*find_function(const char *format)(va_list));
+/**
+ * struct specifier - structure to map format specifiers to functions
+ * @type: format specifier
+ * @func: function associated with the specifier
+ */
+typedef struct specifier
+{
+	const char *type;
+	int (*func)(va_list);
+} specifier_t;
+
+
+int (*find_function(const char *format))(va_list);
 int _putchar(char c);
 int print_char(va_list args);
+int _printf(const char *format,...);
 
 #endif
